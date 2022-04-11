7ac29f5b12db7e082b3a36b14f1ff854a834732b
Rostik <levxarkov@gmail.com>
2022-04-11 11:31:35 +0200

    Added my project

---
+++ bed.py
@@ -0,0 +1,48 @@
+# xoroshego dnia
+#print("bobabob", 3,3,3,3,  "dadada", sep= "", end="!" )
+#print(", lotozabava")
+#print('большая жопа стоит больших потерь-"степан бандера"')
+#print(5 + 3 / 4 + 4)
+#input('и каков твой день сегодня? ')
+
+#numberer = 5
+#print("сколько я раз тебя просил", numberer)
+#del numberer
+
+#pop = 4
+#print("da net", pop)
+
+#ok = "ya tyt"
+#lol = True
+#print(ok, lol)
+
+#ooo = True
+#oo = 3
+#o = "oleg"
+#print (o + str( oo + ooo))
+
+#num1 = int (input("какой день был последний в твоей жизни"))
+#num2 = int (input("это тот самый?"))
+#num1 += 5
+
+#print("результатище", num1 + num2)
+#print("результатище", num1 - num2)
+#print("результатище", num1 * num2)
+#print("результатище", num1 / num2)
+
+num11 = "да ты прав"
+num22 = 3+3*9
+
+input("когда я был на воле?")
+
+print("да прибудет с тобой бог")
+
+print("дед оба дед",  33, end="\n")
+input1 = input("был ты кк то на воле да и закончилось твое путишествие")
+
+print(num11 + " " + str(num22))
+
+
+
+
+
